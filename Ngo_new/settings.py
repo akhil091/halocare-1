a5883870145dc5ad667323ce1a8f3b7d55c42f98
Chetan45s <chetansalmotra45@gmail.com>
2021-10-14 00:31:14 +0530

    adding files

--- Ngo_new/settings.py
+++ Ngo_new/settings.py
@@ -13,7 +13,6 @@ import os
 from pathlib import Path
 import django_heroku
 
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,7 +121,7 @@ PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
@@ -150,3 +149,5 @@ TINYMCE_DEFAULT_CONFIG = {
     "a11ycheck ltr rtl | showcomments addcomment code",
     "custom_undo_redo_levels": 10,
 }
+
+django_heroku.settings(locals())
